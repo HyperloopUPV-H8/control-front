675529c0a0f445761c54b8841d2d6abc3e5a5fd2
lipezaballa <lipezaballa@gmail.com>
2023-04-05 16:53:38 +0200

    Load data

--- src/pages/CamerasPage/Footer/Tube/Tube.tsx
+++ src/pages/CamerasPage/Footer/Tube/Tube.tsx
@@ -1,11 +1,16 @@
 import styles from "./Tube.module.scss";
 import { ReactComponent as TeamLogo } from "assets/svg/team_logo.svg";
 import { Kenos } from "./Kenos/Kenos";
+import { useMeasurements } from "pages/VehiclePage/useMeasurements";
 
 const SECTIONS_BY_SIDE = 6;
 
 export const Tube = () => {
     const sectionsArray = new Array(SECTIONS_BY_SIDE).fill(0);
+    const measurements = useMeasurements();
+    const pos = measurements["position"].value as number;
+    console.log(pos);
+    //console.log(measurements);
 
     return (
         <div className={styles.tube}>
@@ -19,7 +24,7 @@ export const Tube = () => {
                 return <div key={index} className={styles.rightSection} />;
             })}
             {/* //TODO: Change position to a measurement with addUseSelector */}
-            <Kenos position={20} />
+            <Kenos position={pos} />
         </div>
     );
 };
--- src/pages/CamerasPage/camerasRoute.tsx
+++ src/pages/CamerasPage/camerasRoute.tsx
@@ -1,6 +1,8 @@
+import { loadPodData } from "pages/VehiclePage/loadPodData";
 import { CamerasPage } from "./CamerasPage";
 
 export const camerasRoute = {
     path: "/cameras",
+    loader: loadPodData,
     element: <CamerasPage />,
 };
